8f21f8fa6bc5655dabb41e4fa0c9cab7bb4c44ba
pagefile <ahgaudette@gmail.com>
2017-04-23 21:26:27 -0500

    Basic objective functionality
    Objectives can be completed and the gamemode is notified (as long as
    they are added in the editor)

---
+++ Assets/Scripts/DestroyObjective.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DestroyObjective : Objective, IDestroyedEventTarget
+{
+    // Internal
+    private Destructable objective = null;
+
+	// Use this for initialization
+	void Start () 
+    {
+        objective = GetComponent<Destructable>();
+        if(objective == null)
+        {
+            // It can't be destroyed. No bueno
+            OnUpdateObjective(Objective.State.Failure);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // IDestroyedEventTarget
+    public void OnDestroyed()
+    {
+        OnUpdateObjective(Objective.State.Success);
+    }
+}
--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -3,29 +3,81 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class GameMode : MonoBehaviour {
-
+// I need to change either how this handles the objectives or how DestroyObjective works, since currently
+// the DestroyObjective component is destroyed with the object. Not the best thing to do
+public class GameMode : MonoBehaviour
+{
     [SerializeField]
-    private Text StatusText;
+    List<Objective> ObjectiveList;
 
-    // HACK: The player should be spawned by the game mode and kept track of that way....
-    // But this way is easier...and gross
-    [SerializeField]
-    ToyFighter Player;
+    // HACK: quick and dirty. Like a hobo
+    static private GameMode _instance = null;
+    static public GameMode Instance
+    {
+        get { return _instance; }
+        private set { }
+    }
+
+    // Internal
+    int totalCriticalSuccess = 0; // HACK: Hackish, but not nearly as much as what I had before
+    int currentCirticalSuccess = 0;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+		if(_instance == null)
+        {
+            // It gets the job done...
+            _instance = this;
+        }
+
+        // Listen to objective events
+        for(int i = 0; i < ObjectiveList.Count; i++)
+        {
+            Objective obj = ObjectiveList[i];
+            if (obj.IsCritical)
+            {
+                totalCriticalSuccess++;
+            }
+            obj.UpdateObjective += UpdateObjective;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if(StatusText)
+        // So...this is terrible, and I hate it, but I don't want to spend too much
+        // time designing a more robust way to handle this.
+        // I feel like ideally I would use the event system, but since I have work on
+        // two days of the jam I really don't have the time to read up on it :(
+        bool missionPassed = true;  // ick
+        for(int i = 0; i < ObjectiveList.Count; i++)
         {
-            float speed = Player.GetComponent<Rigidbody>().velocity.magnitude;
-            StatusText.text = "Speed: " + speed.ToString("0.00");
+            Objective obj = ObjectiveList[i];
+            if(obj.IsCritical && obj.CurrentState == Objective.State.Failure)
+            {
+                // do some sort of game over thing
+            }
+            else if(obj.IsCritical && obj.CurrentState == Objective.State.Active)
+            {
+                missionPassed = false; // ick
+            }
+        }
+        if(missionPassed == true)   // ick
+        {
+            // do some sort of victory dance thing
         }
 	}
+
+    public void UpdateObjective(Objective obj, Objective.State state)
+    {
+        if(state == Objective.State.Success && obj.IsCritical)
+        {
+            currentCirticalSuccess++;
+        }
+        if(currentCirticalSuccess == totalCriticalSuccess)
+        {
+            Debug.Log("You win!");
+        }
+    }
 }
