b3fbe30d8caf3e01519dfce28a2a5af2a954aab7
pagefile <ahgaudette@gmail.com>
2017-05-03 23:31:42 -0500

    more janitor
    added more comment

--- Assets/Scripts/System/PlayerController.cs
+++ Assets/Scripts/System/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
 
     // HACK: Kinda hacky, but it'll work for now. I just want to decouple the Input manager
     // calls from the objects so technically anything can control them without a major
-    // refactor
+    // refactor. At the very least, Target should be a field or something public so it
+    // can be changed by code for different reasons (character change, character spawn,
+    // joining a network game, things like that)
 
     [SerializeField]
     private InputComponent Target;
