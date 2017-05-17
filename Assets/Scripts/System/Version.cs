60f0e876451a4de860674f5923df3841e51e9f58
pagefile <ahgaudette@gmail.com>
2017-05-16 20:57:11 -0500

    Added game version to code
    Cause Unity editor doesn't have it...

---
+++ Assets/Scripts/System/Version.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Version
+{
+    // This is dumb, but the editor has no global version variable to edit
+    public int Major = 0;
+    public int Minor = 1;
+    public int Internal = 0;
+    
+    public string Full
+    {
+        get { return Major + "." + Minor + "." + Internal; }
+    }
+}
